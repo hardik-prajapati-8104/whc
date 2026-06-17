75ff095b43c23bb318067bbc3132e93e26afb62c
hardik prajapati <hardikprajapati8104@gmail.com>
2026-06-17 17:11:12 +0530

    Initial project upload

--- js/main.js
+++ js/main.js
@@ -100,3 +100,25 @@ if (contactForm) {
         }
     }
 });  
+ 
+
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", function() {
+
+    if (window.pageYOffset > 300) {
+        scrollTopBtn.classList.add("show");
+    } else {
+        scrollTopBtn.classList.remove("show");
+    }
+
+});
+
+scrollTopBtn.addEventListener("click", function() {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+}); 
